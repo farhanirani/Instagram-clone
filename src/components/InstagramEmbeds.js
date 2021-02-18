f937a3744d228f0925e4443ffe2671c7c0b4e385
Farhan Irani <farhanirani007@gmail.com>
2021-02-18 12:25:58 +0530

    rie

--- src/components/InstagramEmbeds.js
+++ src/components/InstagramEmbeds.js
@@ -2,10 +2,25 @@ import React from "react";
 import InstagramEmbed from "react-instagram-embed";
 
 function InstagramEmbeds() {
+  const qwe =
+    "32090f07adc24893fb3158f360841b2772b148063f8513bf39842cda70f09023";
+  const asd = "393139286007145541700682931393";
+  const [qweqwe, asdasd] = [...qwe].reduce(
+    (r, char, i) => (r[i % 2].push(char), r),
+    [[], []]
+  );
+  const [qweqweqwe, asdasdasd] = [...asd].reduce(
+    (r, char, i) => (r[i % 2].push(char), r),
+    [[], []]
+  );
+  const cat = "" + qweqweqwe.join("") + "|" + qweqwe.join("");
+  const catt = "" + asdasd.join("") + "|" + asdasdasd.join("");
+
   return (
     <div className="right__side">
       <InstagramEmbed
         url="https://www.instagram.com/p/C/?hl=en"
+        clientAccessToken={cat}
         maxWidth={320}
         hideCaption={false}
         containerTagName="div"
@@ -19,6 +34,7 @@ function InstagramEmbeds() {
 
       <InstagramEmbed
         url="https://www.instagram.com/p/BqGHnxPBkPj/"
+        clientAccessToken={cat}
         maxWidth={320}
         hideCaption={false}
         containerTagName="div"
@@ -31,6 +47,7 @@ function InstagramEmbeds() {
       />
       <InstagramEmbed
         url="https://www.instagram.com/p/B4TSlGxl0iy/"
+        clientAccessToken={cat}
         maxWidth={320}
         hideCaption={false}
         containerTagName="div"
@@ -43,6 +60,7 @@ function InstagramEmbeds() {
       />
       <InstagramEmbed
         url="https://www.instagram.com/p/B7xOgFjnuiO/"
+        clientAccessToken={cat}
         maxWidth={320}
         hideCaption={false}
         containerTagName="div"
@@ -55,6 +73,7 @@ function InstagramEmbeds() {
       />
       <InstagramEmbed
         url="https://www.instagram.com/p/CFsNZqKFTDx/"
+        clientAccessToken={cat}
         maxWidth={320}
         hideCaption={false}
         containerTagName="div"
@@ -67,6 +86,7 @@ function InstagramEmbeds() {
       />
       <InstagramEmbed
         url="https://www.instagram.com/p/B1V5m7WFfUy/"
+        clientAccessToken={cat}
         maxWidth={320}
         hideCaption={false}
         containerTagName="div"
